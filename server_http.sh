d97fa8d3d337e6e4c4f3c3e5a146faf306401a1f
bublik77 <andrii.bobrovych@gmail.com>
2020-09-23 15:31:15 +0300

    modify

--- server_http.sh
+++ server_http.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 INDEX="./index.html"
 
-while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; cat $INDEX; } | nc -l 8080; done
+while true; do echo -e 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello world!' | nc -l 8080; done
 
